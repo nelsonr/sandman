73f306305e4a4a51286c665035b6a925dcd03950
Nelson Rodrigues <nelsonrodrigues@hi-interactive.com>
2025-03-14 23:32:16 +0000

    Add some comments; Change title of index.html file

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 	"path"
 )
 
+// Directory and file permissions
 const DIR_PERM = 0755
 const FILE_PERM = 0644
+
+// The "embed" directives set the contents of the files
+// into the variables below during compile time.
+// Paths must be relative to this file.
 
 //go:embed assets/index.html
 var htmlFile []byte
@@ -22,19 +27,24 @@ var jsFile []byte
 func main() {
 	args := os.Args
 
-	if len(args) == 1 {
-		fmt.Printf("Usage: sandman SANDBOX_NAME\n")
+	// Show help message and exit when the provided number of arguments is wrong
+	if len(args) != 2 {
+		fmt.Printf("Usage: sandman NAME\n")
 		os.Exit(0)
 	}
 
+	// Name of the directory to be created
 	name := args[1]
 
+	// Attempts to create the named directory
+	// Fails and exits if there's already one with the same name
 	err := os.Mkdir(name, DIR_PERM)
 	if err != nil {
 		fmt.Printf("A directory with the same name already exists: %s\n", name)
 		os.Exit(0)
 	}
 
+	// Creates the files inside the sandbox directory
 	err = os.WriteFile(path.Join(name, "index.html"), []byte(htmlFile), FILE_PERM)
 	if err != nil {
 		panic(err)
@@ -50,5 +60,6 @@ func main() {
 		panic(err)
 	}
 
+	// Displays a success message
 	fmt.Printf("Sandbox \"%s\" created with success!", name)
 }
